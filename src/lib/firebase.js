f8eedbef7db46780f03e4fc16f33ac08372f99cf
Tomiwa Samuel <Createev69@gmail.com>
2025-11-20 15:19:00 +0100

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,26 +4,23 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
-
 const firebaseConfig = {
-apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBGx6-gTqqujwEYQ7jwNR3rcVlfwTuPJkA",
+  authDomain: "nexadev-learning-app.firebaseapp.com",
+  projectId: "nexadev-learning-app",
+  storageBucket: "nexadev-learning-app.firebasestorage.app",
+  messagingSenderId: "98238163582",
+  appId: "1:98238163582:web:29870216caaec0cd8fc62f",
+  measurementId: "G-GGFQW3WYTC"
 };
-
 
 let app;
 if (!getApps().length) {
-app = initializeApp(firebaseConfig);
+  app = initializeApp(firebaseConfig);
 }
-
 
 export const auth = getAuth();
 export const db = getFirestore();
 export const storage = getStorage();
-
 
 export default app;
